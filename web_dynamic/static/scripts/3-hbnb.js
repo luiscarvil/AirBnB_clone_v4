be8544cd19e01e7f4edb341643b9b25492ee5811
Fabkar <fab_862@hotmail.com>
2021-02-14 10:17:27 -0500

    update app and add Fetch places in javascript file

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -28,10 +28,22 @@ $.ajax({
   dataType: 'json',
   contentType: 'application/json',
   success: function (data) {
-    console.log(data);
-    /**
     for (let i = 0; i < data.length; i++) {
+      let place = data[i];
+      $('.places').append(`<article>
+        <div class="title_box">
+          <h2> ${ place.name }</h2>
+          <div class="price_by_night">${place.price_by_night}</div>
+        </div>
+        <div class="information">
+          <div class="max_guest">${place.max_guest} </div>
+                <div class="number_rooms">${place.number_rooms} </div>
+                <div class="number_bathrooms"> ${place.number_bathrooms}</div>
+        </div>
+              <div class="description">
+              ${ place.description}
+              </div>
+      </article>`);
     }
-    */
   }
 });
